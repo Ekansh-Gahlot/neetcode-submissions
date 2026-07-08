018be774ddaa57c88d39f7e6e38f35c9ef874f46
Ekansh Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2026-07-08 23:36:25 +0530

    Add: word-break - submission-17

---
+++ Data Structures & Algorithms/word-break/submission-17.java
@@ -0,0 +1,26 @@
+class Solution {
+    HashMap<Integer, Boolean> map = new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return dfs(s, wordDict, 0);
+    }
+
+    public boolean dfs(String s, List<String> wordDict, int i){
+        if(i == s.length())
+        return true;
+
+        if(map.containsKey(i))
+        return map.get(i);
+
+        for(String w: wordDict){
+            int n = w.length();
+            if((i+n) <= s.length() && s.substring(i, i+n).equals(w))
+            if(dfs(s, wordDict, i+n))
+            {
+                map.put(i, true);
+                return true;
+            }
+        }
+        map.put(i, false);
+        return false;
+    }
+}
